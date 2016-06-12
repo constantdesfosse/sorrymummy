42dbeb2c842425d2322a56e2a30a83422301de49
Constant Desfosse <desfosse.constant@gmail.com>
2016-06-12 17:45:46 +0200

    add autenticate user

--- app/assets/javascripts/tattoos.js
+++ app/assets/javascripts/tattoos.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {
-  $('.showhours').click(function() {
-     $('(".tattoos-card-infos').slideToggle({ direction: "up" }, 300);
-     $(this).toggleClass('clientsClose');
-  }); // end click
+$('.cloudinary-fileupload').bind('cloudinarydone', function(e, data) {
+  $('.preview').html(
+    $.cloudinary.image(data.result.public_id,
+      { format: data.result.format, version: data.result.version,
+        crop: 'fill', width: 150, height: 100 })
+  );
+  $('.image_public_id').val(data.result.public_id);
+  return true;
 });
