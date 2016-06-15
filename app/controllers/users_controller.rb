54a90df80432569da093b5ece36d46c14bb6ac2b
Constant Desfosse <desfosse.constant@gmail.com>
2016-06-15 03:16:35 +0200

    adding gmap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all
+    @users = User.all.where(provider: nil).order(created_at: :desc)
+
+    @markers = Gmaps4rails.build_markers(@users) do |user, marker|
+      marker.lat user.latitude
+      marker.lng user.longitude
+    end
   end
 
 end
