b9ff93eab17dc3ca15c54d25d381af33b43b5b09
Constant Desfosse <desfosse.constant@gmail.com>
2016-06-15 17:09:35 +0200

    fixxxing maps

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all.where(provider: nil).order(created_at: :desc)
 
-    @markerstattooists = Gmaps4rails.build_markers(@users) do |user, marker|
+    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
       marker.infowindow user.first_name + user.last_name
