8f1cb7ed566d693b8a913d3bf771871532edd324
Constant Desfosse <desfosse.constant@gmail.com>
2016-06-15 17:47:09 +0200

    fixing rails4map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,9 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.all.where(provider: nil).order(created_at: :desc)
+    @users = User.all
+
+    #.where(provider: nil).order(created_at: :desc)
 
     @hash = Gmaps4rails.build_markers(@users) do |user, marker|
       marker.lat user.latitude
